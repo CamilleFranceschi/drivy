552f80608129b4a04279e9a28c8421ea6ca30e5e
Camille Franceschi <franceschi.camille@gmail.com>
2017-06-26 13:28:00 +0200

    level 3 done

--- app/controllers/api/v1/rentals_controller.rb
+++ app/controllers/api/v1/rentals_controller.rb
@@ -3,15 +3,16 @@ class Api::V1::RentalsController < Api::V1::BaseController
   def index
     cars = policy_scope(Car)
     @rentals = []
+    @commissions = []
     cars.each do |car|
       car.rentals.each do |rental|
         output = {}
         output[:id] = rental.id
-        car_hash = car.attributes
+        price_per_km = rental.car.price_per_km
+        distance = rental.distance
+
         number_of_days = (rental.end_date - rental.start_date + 1).to_i
         days_array = (1..number_of_days).to_a
-        distance = rental.distance
-        price_per_km = rental.car.price_per_km
         total_daily_amount = days_array.map do |day|
           case day
           when 0..1
@@ -19,13 +20,20 @@ class Api::V1::RentalsController < Api::V1::BaseController
           when 2..4
             rental.car.price_per_day * 0.9
           when 5..10
-          rental.car.price_per_day * 0.7
+            rental.car.price_per_day * 0.7
           else
-          rental.car.price_per_day * 0.5
+            rental.car.price_per_day * 0.5
           end
         end
-        rental_price = (total_daily_amount.reduce(0, :+) + distance * price_per_km).to_f
-        output[:price] = rental_price
+
+        rental_price = total_daily_amount.reduce(0, :+) + distance * price_per_km
+        output[:price] = rental_price.to_f
+        commissions_hash = {}
+        commissions_hash[:insurance_fee] = (0.3 * rental_price / 2).to_f
+        commissions_hash[:assistance_fee] = (number_of_days * 100).to_f
+        commissions_hash[:drivy_fee] = (commissions_hash[:insurance_fee] - commissions_hash[:assistance_fee]).to_f
+
+        output[:commission] = commissions_hash
         @rentals << output
       end
     end
--- spec/requests/rental_management_spec.rb
+++ spec/requests/rental_management_spec.rb
@@ -11,9 +11,22 @@ RSpec.describe 'Rental Management', type: :request do
       rental2 = Rental.create!(car: car1, start_date: Date.new(2050, 3, 31), end_date: Date.new(2050, 4, 1), distance: 300)
       rental3 = Rental.create!(car: car1, start_date: Date.new(2050, 7, 3), end_date: Date.new(2050, 7, 14), distance: 1000)
       get "/api/v1/rentals"
-      expect(response.body).to include("\"price\":3000")
-      expect(response.body).to include("\"price\":6800")
-      expect(response.body).to include("\"price\":27800")
+      expect(response.body).to include("\"price\":3000.0")
+      expect(response.body).to include("\"price\":6800.0")
+      expect(response.body).to include("\"price\":27800.0")
+    end
+
+    it "shows well calculated commissions" do
+      Rental.destroy_all
+      Car.destroy_all
+      car1 = Car.create!(price_per_day: 2000, price_per_km: 10)
+      rental1 = Rental.create!(car: car1, start_date: Date.new(2050, 12, 8), end_date: Date.new(2050, 12, 8), distance: 100)
+      rental2 = Rental.create!(car: car1, start_date: Date.new(2050, 3, 31), end_date: Date.new(2050, 4, 1), distance: 300)
+      rental3 = Rental.create!(car: car1, start_date: Date.new(2050, 7, 3), end_date: Date.new(2050, 7, 14), distance: 1000)
+      get "/api/v1/rentals"
+      expect(response.body).to include("\"price\":3000.0,\"commission\":{\"insurance_fee\":450.0,\"assistance_fee\":100.0,\"drivy_fee\":350.0}")
+      expect(response.body).to include("\"price\":6800.0,\"commission\":{\"insurance_fee\":1020.0,\"assistance_fee\":200.0,\"drivy_fee\":820.0}")
+      expect(response.body).to include("\"price\":27800.0,\"commission\":{\"insurance_fee\":4170.0,\"assistance_fee\":1200.0,\"drivy_fee\":2970.0}")
     end
   end
 end
