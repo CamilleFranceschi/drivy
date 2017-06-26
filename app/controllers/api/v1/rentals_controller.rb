2ef1d7e7c59da11df752921c0e120a38e88f12e1
Camille Franceschi <franceschi.camille@gmail.com>
2017-06-26 12:19:42 +0200

    level 2 done

--- app/controllers/api/v1/rentals_controller.rb
+++ app/controllers/api/v1/rentals_controller.rb
@@ -8,11 +8,23 @@ class Api::V1::RentalsController < Api::V1::BaseController
         output = {}
         output[:id] = rental.id
         car_hash = car.attributes
-        number_of_days = rental.end_date - rental.start_date + 1
+        number_of_days = (rental.end_date - rental.start_date + 1).to_i
+        days_array = (1..number_of_days).to_a
         distance = rental.distance
         price_per_km = rental.car.price_per_km
-        price_per_day =  rental.car.price_per_day
-        rental_price = (number_of_days * price_per_day + distance * price_per_km).to_f
+        total_daily_amount = days_array.map do |day|
+          case day
+          when 0..1
+            rental.car.price_per_day
+          when 2..4
+            rental.car.price_per_day * 0.9
+          when 5..10
+          rental.car.price_per_day * 0.7
+          else
+          rental.car.price_per_day * 0.5
+          end
+        end
+        rental_price = (total_daily_amount.reduce(0, :+) + distance * price_per_km).to_f
         output[:price] = rental_price
         @rentals << output
       end
--- spec/requests/rental_management_spec.rb
+++ spec/requests/rental_management_spec.rb
@@ -7,14 +7,13 @@ RSpec.describe 'Rental Management', type: :request do
       Rental.destroy_all
       Car.destroy_all
       car1 = Car.create!(price_per_day: 2000, price_per_km: 10)
-      car2 = Car.create!(price_per_day: 3000, price_per_km: 15)
-      rental1 = Rental.create!(car: car1, start_date: Date.new(2017, 12, 8), end_date: Date.new(2017, 12, 10), distance: 100)
-      rental2 = Rental.create!(car: car1, start_date: Date.new(2017, 12, 14), end_date: Date.new(2017, 12, 18), distance: 550)
-      rental3 = Rental.create!(car: car2, start_date: Date.new(2017, 12, 8), end_date: Date.new(2017, 12, 10), distance: 150)
+      rental1 = Rental.create!(car: car1, start_date: Date.new(2050, 12, 8), end_date: Date.new(2050, 12, 8), distance: 100)
+      rental2 = Rental.create!(car: car1, start_date: Date.new(2050, 3, 31), end_date: Date.new(2050, 4, 1), distance: 300)
+      rental3 = Rental.create!(car: car1, start_date: Date.new(2050, 7, 3), end_date: Date.new(2050, 7, 14), distance: 1000)
       get "/api/v1/rentals"
-      expect(response.body).to include("\"price\":7000")
-      expect(response.body).to include("\"price\":15500")
-      expect(response.body).to include("\"price\":11250")
+      expect(response.body).to include("\"price\":3000")
+      expect(response.body).to include("\"price\":6800")
+      expect(response.body).to include("\"price\":27800")
     end
   end
 end
